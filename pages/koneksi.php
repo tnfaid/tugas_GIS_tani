5c06c5e8ab5073e3301be5bc184982a423073574
tnfaid <nurfaidahtitik@gmail.com>
2019-01-21 00:05:13 +0700

    itu udah g kepake :v

--- pages/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$database = "database_sistem";
-
-$koneksi = mysql_connect($host, $user, $pass, $database) or die("Koneksi ke database gagal!");
-mysql_select_db($database, $koneksi) or die("Tidak ada database yang dipilih!");
-?>
